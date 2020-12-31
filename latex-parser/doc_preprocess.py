41043bac6d4555859fcb94949960906d0d293a45
Connor Barlow <barlowc2@wwu.edu>
2020-12-31 15:55:45 -0800

    Created initial macro expansion with recursion

--- latex-parser/doc_preprocess.py
+++ latex-parser/doc_preprocess.py
@@ -7,75 +7,125 @@ Expand citations
 '''
 
 '''
+Returns a TexSoup node representing doc with all macros defined with newcommand 
+  and renewcommand expanded. Handles recursive definitions and all that jazz. 
+  doc itself is modified as a side effect and so should not be used after invocation
+  of this function.
 '''
 def expandDocMacros(doc):
-    def argToString(arg):
-        string = ''
-        for val in arg.all:
-            string += str(val)
-        return string
-
     class Macro:
-        def __init__(self, macroDefn):
+        def __init__(self, macroDefn, prevMacros):
             if macroDefn.name == 'newcommand' or macroDefn.name == 'renewcommand':
                 self.name = macroDefn.args[0].contents[0].name
+                if self.name in prevMacros:
+                    prevMacros.pop(self.name)
+
+                if len(prevMacros) > 0:
+                    self._normalize(macroDefn, prevMacros.values())
+                
                 self.args = 0
                 self.default = None
                 if isinstance(macroDefn.args[1], TexSoup.data.BracketGroup):
                     self.args = int(macroDefn.args[1].contents[0])
                     if isinstance(macroDefn.args[2], TexSoup.data.BracketGroup):
-                        self.default = argToString(macroDefn.args[2])
-                self.defn = argToString(macroDefn.args[-1])
+                        self.default = self._argToString(macroDefn.args[2])
+                self.defn = self._argToString(macroDefn.args[-1])
+                self.position = macroDefn.position
 
+        def _argToString(self, arg):
+            string = ''
+            for val in arg.all:
+                string += str(val)
+            return string
+
+        '''Normalizes the macro so it is defined only in terms of itself'''
+        def _normalize(self, searchTarget, prevMacros):
+            excl = []
+            for arg in searchTarget.args:
+                self._normalize(arg, prevMacros)
+                excl.extend(arg.children)
+            for i, child in enumerate(searchTarget.children):
+                if child not in excl:
+                    if isinstance(child, TexSoup.data.TexEnv):
+                        self._normalize(child, prevMacros)
+                    else:
+                        for macro in prevMacros:
+                            if child.name == macro.name:
+                                self._normalize(child, prevMacros)
+                                searchTarget.insert(i, macro.expandMacro(child))
+                                searchTarget.remove(child)
+
+        '''Returns a string representing the expansion of an invocation of this macro'''
         def expandMacro(self, macro):
             expansion = self.defn
             if self.name == macro.name:
                 refArg = 0
                 targArg = 1
                 if self.default:
-                    targArg += 1
                     expansion = expansion.partition('#1')
                     if len(macro.args) > 0 and isinstance(macro.args[0], TexSoup.data.BracketGroup):
+                        expansion = expansion[0] + self._argToString(macro.args[refArg]) + expansion[2]
                         refArg += 1
-                        expansion = expansion[0] + argToString(macro.args[0]) + expansion[2]
                     else:
                         expansion = expansion[0] + self.default + expansion[2]
+                    targArg += 1
                 for arg in range(targArg, self.args + 1):
                     expansion = expansion.partition('#' + str(arg))
                     if refArg < len(macro.args):
-                        expansion = expansion[0] + argToString(macro.args[refArg]) + expansion[2]
+                        expansion = expansion[0] + self._argToString(macro.args[refArg]) + expansion[2]
                         refArg += 1
                     # Covers both the case that the arg isn't found or it was and needs to be replaced
                     else:
                         expansion = expansion[0] + expansion[2]
             return expansion
-    
-    obj = Macro(doc.newcommand)
-    print(obj.expandMacro(doc.a))
 
-expandDocMacros(TexSoup.TexSoup(r'\newcommand{\a}[2][d]{\b #1 #2}\a{c}'))
-    # bindings = {}
-    # positions = {}
-    # for macro in doc.find_all('newcommand'):
-    #     bindings[macro.args[0].name] = [macro]
-    #     positions[macro.position] = macro
-    # for macro in doc.find_all('renewcommand'):
-    #     if bindings[macro.args[0].name]:
-    #         bindings[macro.args[0].name].append(macro)
-    #     else:
-    #         bindings[macro.args[0].name] = [macro]
-    #     positions[macro.position] = macro
+    def createMacroBindings():
+        defnBindings = {}
+        for macroDefn in doc.find_all('newcommand'):
+            defnBindings[macroDefn.args[0].position] = macroDefn
+        for macroDefn in doc.find_all('renewcommand'):
+            defnBindings[macroDefn.args[0].position] = macroDefn
 
-    # # Expand the bindings relative to one another
-    # bindingOrder = list(positions.keys())
-    # bindingOrder.sort()
-    # with [] as activeBindings:
-    #     for pos in bindingOrder:
-            
+        bindingOrder = list(defnBindings.keys())
+        bindingOrder.sort()
 
-    # # Expand bindings within the text
-    # for name in bindings.keys():
-    #     activeBinding = -1
-    #     for macro in doc.find_all(name):
-    #         while activeBinding + 1 < len(bindings[name]) and bindings[name][activeBinding + 1].position < macro.position:
-    #             activeBinding += 1 
+        # Expand the bindings relative to one another
+        nonlocal macroBindings
+        activeBindings = {}
+        for pos in bindingOrder:
+            macro = Macro(defnBindings[pos], activeBindings)
+            activeBindings[macro.name] = macro
+            if macro.name in macroBindings:
+                macroBindings[macro.name].append(macro)
+            else:
+                macroBindings[macro.name] = [macro]
+
+    def expandDocMacrosSub(node):
+        nonlocal macroBindings
+        excl = []
+        for arg in node.args:
+            expandDocMacrosSub(arg)
+            excl.extend(arg.children)
+        for i, child in enumerate(node.children):
+            if child not in excl:
+                if isinstance(child, TexSoup.data.TexEnv):
+                    expandDocMacrosSub(child)
+                else:
+                    for name in macroBindings.keys():
+                        if child.name == name:
+                            macro = -1
+                            # child.position is valid since the position field of TexNode's remain unchanged even if the doc is modified
+                            while macro+1 < len(macroBindings[name]) and macroBindings[name][macro+1].position < child.position:
+                                macro += 1
+                            if macro >= 0:
+                                node.insert(i, macroBindings[name][macro].expandMacro(child))
+                                node.remove(child)
+
+    macroBindings = {}
+    createMacroBindings()
+    expandDocMacrosSub(doc)
+    return TexSoup.TexSoup(str(doc))
+
+# Basic test
+if __name__ == '__main__':
+    print(expandDocMacros(TexSoup.TexSoup(r'\newcommand{\a}[2][d]{\z #1 #2}\a{a}\a{a}{b}\newcommand{\b}[1]{\a{#1}}\b{b}\renewcommand{\b}[2]{\a[#1]{#2}}\b{b}{c}')))
