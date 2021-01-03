f845978c134f95575bc269099976809729663d85
Taichen <jatr812@gmail.com>
2021-01-03 12:51:10 -0800

    Creating tempates folder

--- webApp/app.py
+++ webApp/app.py
@@ -1,6 +1,28 @@
+# Run flask app: python3 -m flask run
+
 from flask import Flask
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
