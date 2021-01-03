cd0d389b3134f7c0b8fa4eabb23d7b83c4ae2b91
willsower <>
2021-01-02 16:49:06 -0800

    Changing upload to form box

---
+++ webApp/scripts/main.py
@@ -0,0 +1,11 @@
+import os 
+  
+fileitem = form['filename'] 
+  
+# check if the file has been uploaded 
+if fileitem.filename: 
+    # strip the leading path from the file name 
+    fn = os.path.basename(fileitem.filename) 
+      
+   # open read and write the file into the server 
+    open(fn, 'wb').write(fileitem.file.read()) 
