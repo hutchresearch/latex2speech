c9de454874c83c4fc5bd488bbd1e7bbaad972426
Taichen <jatr812@gmail.com>
2021-01-03 12:54:37 -0800

    Updating file

--- webApp/app.py
+++ webApp/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from datetime import datetime
+from flask import render_template
 import re
 
 app = Flask(__name__)
@@ -10,19 +11,11 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
