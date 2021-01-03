69da3418e6b561c17c2a92c09bb9e240326d9cad
Taichen <jatr812@gmail.com>
2021-01-03 15:05:18 -0800

    Wrote function to change .tex file to .ssml

--- webApp/aws_polly_render.py
+++ webApp/aws_polly_render.py
@@ -1,3 +1,12 @@
+import os
+
+# Changes .tex file to SSML file
+def change_file_type(file):
+    base = os.path.splitext(file)[0]
+    os.rename(file, base + '.ssml')
+
+    # Feed SSML file to amazon polly 
+
 # Function that is called from app.py with file
 # Adds beginner and end SSML tags
 def start_polly(file):
@@ -7,3 +16,9 @@ def start_polly(file):
         f.seek(0, 0)
         beginTag = "<speak>"
         f.write(beginTag.rstrip('\r\n') + '\n' + content + '\n</speak>')
+
+    # Call parser here
+    # newFile = start_parser(file)
+
+    # Change .tex file to .SSML file here
+    
