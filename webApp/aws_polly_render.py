cb88082c7243c588db36a5cadeec807b983eab42
Taichen <jatr812@gmail.com>
2021-01-03 15:00:28 -0800

    Got ssml tags at beginning and end of file

--- webApp/aws_polly_render.py
+++ webApp/aws_polly_render.py
@@ -1,6 +1,9 @@
-
-
 # Function that is called from app.py with file
 # Adds beginner and end SSML tags
 def start_polly(file):
-    print(file)
+    # Opens file adds tag to begin and end
+    with open(file, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        beginTag = "<speak>"
+        f.write(beginTag.rstrip('\r\n') + '\n' + content + '\n</speak>')
