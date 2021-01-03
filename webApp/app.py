9f301cdaed48e5f183e7365f8252a8bde89bfea2
Taichen <jatr812@gmail.com>
2021-01-03 13:33:04 -0800

    Got rid of imports that are unnecessary

--- webApp/app.py
+++ webApp/app.py
@@ -1,9 +1,7 @@
 # Run flask app: python3 -m flask run
 
 from flask import Flask
-from datetime import datetime
 from flask import render_template
-import re
 
 app = Flask(__name__)
 
@@ -12,12 +10,3 @@ def home():
     return render_template(
         "main.html"
     )
-
-# @app.route("/hello/")
-# @app.route("/hello/<name>")
-# def hello_there(name = None):
-#     return render_template(
-#         "main.html",
-#         name=name,
-#         date=datetime.now()
-#     )
