56b923913f7ed60553662df1b2b775492f52c688
Taichen <jatr812@gmail.com>
2021-01-03 13:58:32 -0800

    Changed from class to id

--- webApp/static/main.js
+++ webApp/static/main.js
@@ -1,10 +1,10 @@
 const input = document.getElementById('uploadFile');
-const uploadButton = document.getElementsByClassName('uploadButton');
+const uploadButton = document.getElementById('uploadButton');
 
 // Triggers other functions once .tex file
 // has been uploaded
 input.addEventListener('change', function () {
-    
+
     // Show upload button
     uploadButton.style.display = "block";
 
