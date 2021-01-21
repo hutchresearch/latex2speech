3745b2e05305ad4de9bb158bc4738876d355f86e
Taichen <jatr812@gmail.com>
2021-01-21 15:33:56 -0800

    Changing directory format for flask app

--- app/parser_manager.py
+++ app/parser_manager.py
@@ -1,11 +1,13 @@
 import os
+import sys
+sys.path.insert(1, "/Users/taichen/Desktop/Tai/Tex2Speech/latex2speech/app/latex-parser")
+import tex_parser
 
 def start_parsing(file):
-    print("\n\nTEST")
-
-    print(file.filename)
-    os.system("pdflatex " + file)
-
+    # os.system("pdflatex " + file)
+    latex_Parser = tex_parser.TexParser()
+    print("TEST HERE")
+    # tex_parser.parse(file)
     # TODO
     # Get .aux file
     return file
