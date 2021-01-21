a8a28cfe11e918eb18d2144db6d3b10a4f9a2241
Taichen <jatr812@gmail.com>
2021-01-21 15:53:12 -0800

    Commiting for branch purposes

--- app/parser_manager.py
+++ app/parser_manager.py
@@ -1,13 +1,11 @@
 import os
-import sys
-sys.path.insert(1, "/Users/taichen/Desktop/Tai/Tex2Speech/latex2speech/app/latex-parser")
-import tex_parser
+from tex_parser import TexParser
 
 def start_parsing(file):
     # os.system("pdflatex " + file)
-    latex_Parser = tex_parser.TexParser()
+    latex_parser = TexParser()
     print("TEST HERE")
-    # tex_parser.parse(file)
+    latex_parser.parse(file)
     # TODO
     # Get .aux file
     return file
