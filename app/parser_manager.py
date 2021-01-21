725511bb04ad4cbdc78c9ebbe8d3c7e4bf5db43e
Taichen <jatr812@gmail.com>
2021-01-21 12:23:20 -0800

    Ignore

--- app/parser_manager.py
+++ app/parser_manager.py
@@ -1,4 +1,10 @@
 
 def start_parsing(file):
     print("\n\nTEST")
+
+    # TODO
+    # Connect to Connor's Parser
+    # Get .aux file first
+    # Run through
+    # Ignore math mode for now?
     return file
