c8509be908e923743f300c26f3c394b9927b348e
Taichen <jatr812@gmail.com>
2021-01-28 12:23:10 -0800

    Unit test for basic templates in flask

--- tex2speech/tests/functional/test_flask.py
+++ tex2speech/tests/functional/test_flask.py
@@ -2,18 +2,14 @@ import unittest
 import app
 
 class BasicTestCase(unittest.TestCase):
+        '''Test basic home / download pages'''
+        def test_home(self):
+                tester = app.test_client(self)
+                pages = ['/', 'download']
+                for page in pages:
+                        response = tester.get(page, content_type='html/text')
+                        self.assertEqual(response.status_code, 200)
 
-    def test_home(self):
-            tester = app.test_client(self)
-            response = tester.get('/', content_type='html/text')
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data, b'Hello World!')
-
-    def test_other(self):
-            tester = app.test_client(self)
-            response = tester.get('a', content_type='html/text')
-            self.assertEqual(response.status_code, 200)
-            self.assertTrue(b'does not exist' in response.data)
 
 if __name__ == '__main__':
     unittest.main()
