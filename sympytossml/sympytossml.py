870b1a742ec3282efa9c46391846e401dcb65885
Walker Herring <herrinw>
2021-02-02 01:45:06 -0800

    fixed spaces

--- sympytossml/sympytossml.py
+++ sympytossml/sympytossml.py
@@ -5,10 +5,16 @@ quantity_start = 'begin quantity'
 quantity_end = 'end quantity'
 
 def convert_sympy_ssml(expr):
-    print_tree(expr, assumptions = False)
     funcs_tree = ET.parse('sympy_funcs.xml')
-    return _convert(expr, funcs_tree)
+    s = _convert(expr, funcs_tree)
+    if s[0] == ' ':
+        s = s[1:]
+    if s[len(s) - 1] == ' ':
+        s = s[:(len(s) - 1)]
+    return s
 
+
+    
 def _convert(expr, funcs_tree):
     
     func_id = expr.__class__.__name__
@@ -21,7 +27,7 @@ def _convert(expr, funcs_tree):
         func = r.find('userFunction')
         
     if len(expr.args) == 0:
-        s += str(expr)
+        s += ' ' + str(expr) + ' '
     else:
         i = 0
         j = 0
@@ -35,12 +41,12 @@ def _convert(expr, funcs_tree):
                 if isinstance(expr.args[i], Atom):
                     s += _convert(expr.args[i], funcs_tree)
                 else:
-                    s += quantity_start + _convert(expr.args[i], funcs_tree) + quantity_end
+                    s += ' ' + quantity_start + _convert(expr.args[i], funcs_tree) + quantity_end + ' '
                 i += 1
                 j += 1
             
             elif func[j].tag == 'funcname':
-                s += func_id
+                s += ' ' + func_id + ' '
                 j += 1
 
             elif func[j].tag == 'text':
