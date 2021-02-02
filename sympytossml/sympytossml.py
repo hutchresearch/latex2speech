19e806a74f935d53e864adb7d5e84522eab4e0c4
Walker Herring <herrinw>
2021-02-01 19:50:02 -0800

    added support for many sympy functions. Need to look closer at calculus and summations.

--- sympytossml/sympytossml.py
+++ sympytossml/sympytossml.py
@@ -4,8 +4,8 @@ import xml.etree.ElementTree as ET
 quantity_start = 'begin quantity'
 quantity_end = 'end quantity'
 
-def convert(expr):
-    #print_tree(test_expr, assumptions = False)
+def convert_sympy_ssml(expr):
+    print_tree(expr, assumptions = False)
     funcs_tree = ET.parse('sympy_funcs.xml')
     return _convert(expr, funcs_tree)
 
@@ -16,6 +16,10 @@ def _convert(expr, funcs_tree):
     func = r.find(func_id)
     
     s = str()
+    
+    if func == None:
+        func = r.find('userFunction')
+        
     if len(expr.args) == 0:
         s += str(expr)
     else:
@@ -26,15 +30,19 @@ def _convert(expr, funcs_tree):
         while i < len(expr.args):
             if j >= len(func):
                 j = repeat_index
-
+            
             if func[j].tag == 'arg':
-                if isinstance(expr.args[i], Symbol):
+                if isinstance(expr.args[i], Atom):
                     s += _convert(expr.args[i], funcs_tree)
                 else:
                     s += quantity_start + _convert(expr.args[i], funcs_tree) + quantity_end
                 i += 1
                 j += 1
             
+            elif func[j].tag == 'funcname':
+                s += func_id
+                j += 1
+
             elif func[j].tag == 'text':
                 s += str(func[j].text)
                 j += 1
