62d3031a733b4560e7d5802992b20f4f5b69b5f6
Taichen <jatr812@gmail.com>
2021-02-05 13:18:29 -0800

    Saving file names in array, storing in folder

--- tex2speech/app.py
+++ tex2speech/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, make_response, session, url_for, redirect
 from aws_polly_render import start_polly
 from flask_dropzone import Dropzone
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
@@ -12,7 +13,9 @@ dropzone = Dropzone(app)
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.tex, .bib'
-app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+# app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+# Uploads settings
+app.config['UPLOAD_FOLDER'] = os.getcwd() + '/upload'
 
 app.config['SECRET_KEY'] = 'something_here'
 
@@ -20,41 +23,32 @@ app.config['SECRET_KEY'] = 'something_here'
 def index():
     # set session for results
     if "file_holder" not in session:
-        session['file_holder'] = {}
+        session['file_holder'] = []
 
     if "bilb_holder" not in session:
-        session['bib_holder'] = {}
+        session['bib_holder'] = []
 
     # List to hold our files
     file_holder = session['file_holder']
     bib_holder = session['bib_holder']
-
-    file_holder = {}
-    bib_holder = {}
 
     if request.method == 'POST':
         file_obj = request.files
 
         for f in file_obj:
             file = request.files.get(f)
-            print(file.filename)
-            if os.path.splitext(file.filename)[1] == ".tex":
-                hashObject = []
-                hashObject.append(str(file.read(), 'utf-8'))
-                file_holder[file.filename] = hashObject
-                # print("Tex file")
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
 
+            if os.path.splitext(file.filename)[1] == ".tex":
+                file_holder.append(file.filename)
             elif os.path.splitext(file.filename)[1] == ".bib":
-                # Hashtable of bib files
-                hashObject = []
-                hashObject.append(file.read())
-                bib_holder[os.path.splitext(file.filename)[0]] = hashObject
-                # print("Bib file")
+                bib_holder.append(file.filename)
 
         # Add files to session
         session['file_holder'] = file_holder
         session['bib_holder'] = bib_holder
-        return "uploading..."
+        return redirect(url_for('results'))
+        # return "uploading..."
 
     return render_template(
         'index.html'
@@ -73,33 +67,10 @@ def results():
     audio_links = start_polly(file_holder, bib_holder)
     session.pop('file_holder', None)
     session.pop('bib_holder', None)
+
     return render_template(
         'download.html',
         file_audio = zip(file_holder, audio_links))
-
-# Render home page at start of use
-# @app.route("/")
-# def index():
-#     return render_template(
-#         "index.html"
-#     )
-
-# # Get file after download, feed it to parser
-# # Display download.html file
-# @app.route("/download", methods = ['POST'])
-# def render_then_download():
-#     if request.method == 'POST':
-#         # Gets file, passes file to aws_polly_render
-#         # file = request.files['file']
-#         # audio_link = start_polly(file)
-#         file_obj = request.files
-#         print(len(file_obj))
-#         for f in file_obj:
-#             print(f.filename)
-#         # Displays download page, with audio
-#         return render_template(
-#             "download.html"
-#         )
 
 # If usr tries going to random page on our web application
 # through page does not exist
