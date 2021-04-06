c3dffa14353b1a32ee28ce4bd9037033ba6b7448
Taichen <jatr812@gmail.com>
2021-04-05 19:49:04 -0700

    Edited download header, working on changing button

--- tex2speech/static/main.js
+++ tex2speech/static/main.js
@@ -1,2 +1,13 @@
+const form = document.getElementById('formID');
 
+$( document ).ready(function() {
+    $(function() {
+        $('#submit').click(function(){
+            button = document.getElementById('submit');
+            button.value = 'Processing...';
+            button.style.backgroundColor = '#B8B8B8';
+            // document.getElementById('blocker').style.visibility = 'visible';
+        });
+    });
+});
 
