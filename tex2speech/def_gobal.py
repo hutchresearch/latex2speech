de9b5f500cbcae484d4b1f6dd608c639a3ed36e2
Connor Barlow <conn.barlowr@gmail.com>
2021-04-14 10:56:25 -0700

    Deleting useless files

--- tex2speech/def_gobal.py
+++
@@ -1,5 +0,0 @@
-class DefType:
-    def __init__(self, name, number_args, replacement_string):
-        self.name = name
-        self.number_args = number_args
-        self.replacement_string = replacement_string
