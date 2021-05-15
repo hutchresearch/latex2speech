468255166c7da14b20f8f3c94c083a222f5fc32e
Taichen <jatr812@gmail.com>
2021-05-15 15:41:49 -0700

    Added in content for changing xml file

--- tex2speech/modify_xml.py
+++ tex2speech/modify_xml.py
@@ -1,4 +1,33 @@
 import xml.etree.ElementTree as ET
+import yaml
   
-mytree = ET.parse('test.xmt')
+mytree = ET.parse('./static/test.xml')
 myroot = mytree.getroot()
+
+config_file = open('app_config.yaml')
+contents = yaml.load(config_file, Loader=yaml.FullLoader)
+
+# Helper to modify under cmd
+def modify_xml(family_tag, tagName, setAttr, setVal):
+    for family in mytree.findall('cmd'):
+        if family.attrib['family'] == family_tag:
+            modifiable = family.find(tagName)
+            modifiable.set(setAttr, setVal)
+
+# Bold Tags -> family = 'bold'
+#  \em \emph \textbf
+attr = ''
+
+if contents['BOLD']['CONFIG']['TYPE'] != 'None':
+    type = contents['BOLD']['CONFIG']['TYPE']
+    val = contents['BOLD']['CONFIG']['EMPHASIS']
+else:
+    type = contents['BOLD']['DEFAULT']['TYPE']
+    val = contents['BOLD']['DEFAULT']['EMPHASIS']
+
+if (type == 'emphasis'):
+    attr = 'level'
+
+modify_xml('bold', type, attr, val)
+
+mytree.write('./static/output.xml')
