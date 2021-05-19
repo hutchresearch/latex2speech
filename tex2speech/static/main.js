a59d28f567ce48bd220b7b8f7fa0e28ade09a223
Taichen <jatr812@gmail.com>
2021-05-19 09:56:46 -0700

    Fixed broken code from main, fixed visibility

--- tex2speech/static/main.js
+++ tex2speech/static/main.js
@@ -1,6 +1,7 @@
 // initial state of options
 document.getElementById('advanced_options_box').style.display = 'none';
-document.getElementById('advanced_options_button').textContent = 'Advanced Options \u2BC8'
+document.getElementById('advanced_options_button').textContent = 'Advanced Options \u2BC8';
+updateAdvancedSuboptions();
 
 $( document ).ready(function() {
     // Disable button once form has been submitted
@@ -16,12 +17,35 @@ $( document ).ready(function() {
 
 function toggle_advanced_options() {
     var optBox = document.getElementById('advanced_options_box');
-    var optButton = document.getElementById('advanced_options_button')
+    var optButton = document.getElementById('advanced_options_button');
     if (optBox.style.display === 'none') {
         optBox.style.display = 'block';
-        optButton.textContent =  'Advanced Options \u2BC6'
+        optButton.textContent =  'Advanced Options \u2BC6';
     } else {
         optBox.style.display = 'none';
-        optButton.textContent =  'Advanced Options \u2BC8'
+        optButton.textContent =  'Advanced Options \u2BC8';
+    }
+}
+
+function updateAdvancedSuboptions() {
+    var mathErrorType = document.getElementById('math_error_type');
+    if (mathErrorType.options[mathErrorType.selectedIndex].text == 'Message') {
+        document.getElementsByClassName('math_error_text')[0].style.display = 'table-row'
+        document.getElementsByClassName('math_error_break')[0].style.display = 'none'
+    }
+    
+    if (mathErrorType.options[mathErrorType.selectedIndex].text == 'Break') {
+        document.getElementsByClassName('math_error_text')[0].style.display = 'none'
+        document.getElementsByClassName('math_error_break')[0].style.display = 'table-row'
+    }
+
+    var boldType = document.getElementById('bold_type');
+    if (boldType.options[boldType.selectedIndex].text == 'emphasis') {
+        for (let e of document.getElementsByClassName('bold_emphasis')) { e.style.display = 'table-row'}
+        for (let e of document.getElementsByClassName('bold_prosody')) { e.style.display = 'none'}
+    }
+    if (boldType.options[boldType.selectedIndex].text == 'prosody') {
+        for (let e of document.getElementsByClassName('bold_emphasis')) { e.style.display = 'none'}
+        for (let e of document.getElementsByClassName('bold_prosody')) { e.style.display = 'table-row'}
     }
 }
