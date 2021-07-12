a4aa09cb8bc991c77ef371ffe2a9b1df27c9c152
willsower <jatr812@gmail.com>
2021-07-12 14:15:47 -0400

    Updating tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
-# from tex2speech import application as flask_app
-from tex2speech.application import application
+# from app import application as flask_app
+from app.application import application
 
-# @pytest.fixture
-# def app():
-#     yield flask_app
+@pytest.fixture
+def app():
+    yield flask_app
 
 # @pytest.fixture
 # def client(app):
